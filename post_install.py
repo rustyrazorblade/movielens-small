52989738656efb8759b85f87eba166e7afe2556d
Jon Haddad <jon@jonhaddad.com>
2016-02-25 17:08:18 -0800

    utf 8 encoding

--- post_install.py
+++ post_install.py
@@ -23,16 +23,17 @@ def main(context):
     fp = context.download("http://files.grouplens.org/datasets/movielens/ml-100k.zip")
     zf = ZipFile(file=fp)
     tmp = zf.open("ml-100k/u.item")
-    items = read_csv(tmp, sep="|", header=None, names=["id", "name", "release_date", "video_release_date", "url", "unknown",
-                                                        "Action", "Adventure", "Animation", "Children's", "Comedy", "Crime",
-                                                        "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror",  "Musical",
-                                                        "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])
+    items = read_csv(tmp, sep="|", header=None,
+                    names=[ "id", "name", "release_date", "video_release_date", "url", "unknown",
+                            "Action", "Adventure", "Animation", "Children's", "Comedy", "Crime",
+                            "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror",  "Musical",
+                            "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])
     for row in items.itertuples():
         try:
             Movie.create(id=row.id, name=row.name.encode("utf-8"),
                          url=str(row.url))
-        except:
-            print row
+        except Exception as e:
+            print e, row
 
 if __name__ == "__main__":
     from cdm import install_local
