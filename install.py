595d84866d41ca9a3ded0bea6e1d5d190cfe0eaa
Jon Haddad <jon@jonhaddad.com>
2016-03-01 16:55:28 -0800

    models

--- install.py
+++ install.py
@@ -6,6 +6,22 @@ from cassandra.cqlengine.columns import *
 
 from cdm.installer import Installer
 
+class Movie(Model):
+    __table_name__ = 'movies'
+    id = Integer(primary_key=True)
+    name = Text()
+    release_date = Date()
+    video_release_date = Date()
+    url = Text()
+    tags = Set(Text)
+
+class User(Model):
+    __table_name__ = "users"
+    id = Integer(primary_key=True)
+    age = Integer()
+    gender = Text()
+    occupation = Text()
+    zip = Text()
 
 class MovieLensInstaller(Installer):
     def install_cassandra(self):
